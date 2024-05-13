74d2806b6bcd4b1382d6b70395e26113485e25cb
AndersPanders123 <123541105+AndersPanders123@users.noreply.github.com>
2024-05-13 14:06:49 +0200

    Header
    Ferdig med header som har logo, (Ansatte, Kart og Om oss) og link til sosiale medier.

---
+++ prøveEksamen/src/components/header.tsx
@@ -0,0 +1,45 @@
+import logo from "/logo.png";
+import { CiFacebook } from "react-icons/ci";
+import { CiInstagram } from "react-icons/ci";
+import { CiLinkedin } from "react-icons/ci";
+import { CiTwitter } from "react-icons/ci";
+
+function Header() {
+  return (
+    <div>
+      <div className="header">
+        <img className="header" src={logo} alt="Logo" />
+        <div className="row">
+          <a href="#">
+            <div className="headertxt">Ansatte</div>
+          </a>
+          <a href="#">
+            <div className="headertxt">Kart</div>
+          </a>
+          <a href="#">
+            <div className="headertxt">Om oss</div>
+          </a>
+        </div>
+        <a className="header, icon">
+          <a href="https://www.facebook.com">
+            <CiFacebook size={50} />
+          </a>
+          <a href="https://www.instagram.com">
+            <CiInstagram size={50} />
+          </a>
+          <a href="https://www.linkedin.com">
+            <CiLinkedin size={50} />
+          </a>
+          <a href="https://www.twitter.com">
+            <CiTwitter size={50} />
+          </a>
+        </a>
+      </div>
+      <div>
+        <p></p>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
