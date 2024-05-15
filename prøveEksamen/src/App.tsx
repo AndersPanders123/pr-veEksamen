e0291c36aaac7483d330448b17b093304d3c3d58
AndersPanders123 <123541105+AndersPanders123@users.noreply.github.com>
2024-05-15 11:50:01 +0200

    Cookies
    Cookies er på plass og den virker som den skal, den lagrer seg i cookies isteden for local storage.

--- prøveEksamen/src/App.tsx
+++ prøveEksamen/src/App.tsx
@@ -3,11 +3,13 @@ import Body1 from "./components/body1";
 import Body2 from "./components/body2";
 import Body3 from "./components/body3";
 import Footer from "./components/footer";
+import Cookies from "./components/cookies";
 
 function App() {
   return (
     <div>
       <Header />
+      <Cookies />
       <Body1 />
       <Body2 />
       <Body3 />
---
+++ prøveEksamen/src/components/cookies.tsx
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+
+function Cookies() {
+  const [showCookiePopup, setShowCookiePopup] = useState(false);
+
+  useEffect(() => {
+    const cookiesAccepted = document.cookie
+      .split("; ")
+      .find((row) => row.startsWith("cookiesAccepted"));
+    if (!cookiesAccepted) {
+      setShowCookiePopup(true);
+    }
+  }, []);
+
+  const handleAcceptCookies = () => {
+    document.cookie = "cookiesAccepted=true; max-age=31536000"; // 1 year
+    setShowCookiePopup(false);
+  };
+
+  const handleDeclineCookies = () => {
+    document.cookie = "cookiesAccepted=false; max-age=31536000"; // 1 year
+    setShowCookiePopup(false);
+  };
+
+  return (
+    <>
+      {showCookiePopup && (
+        <div className="cookie-popup">
+          <p>Denne netsiden bruker Cookies</p>
+          <div>
+            <button className="cb" onClick={handleAcceptCookies}>
+              Aksepter
+            </button>
+            <button className="cb" onClick={handleDeclineCookies}>
+              Avslå
+            </button>
+            <p id="lesmer4">Les mer om cookies !</p>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default Cookies;
